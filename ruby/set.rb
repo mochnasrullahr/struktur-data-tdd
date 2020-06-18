5cac98e60d4195284439a75a802914eea89cdb96
Mocha Nas <moch.nasrullah.r@gmail.com>
2020-06-18 07:57:12 +0700

    add Set first starter method

---
+++ ruby/set.rb
@@ -0,0 +1,11 @@
+# Set
+# - kumpulan obyek yang unik
+# - urutan tidak diperhatikan
+
+module StrukturData
+  class Set
+    def empty?
+      true
+    end
+  end
+end
---
+++ ruby/spec/set_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../set'
+
+RSpec.describe StrukturData::Set do
+  it { expect(subject.empty?).to eql true}
+end
