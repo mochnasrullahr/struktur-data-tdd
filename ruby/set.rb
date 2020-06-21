0ad9fd1461ad58fc65acf483f24e64892d0d56ab
Mocha Nas <moch.nasrullah.r@gmail.com>
2020-06-22 06:30:44 +0700

    add some set interfaces

--- ruby/set.rb
+++ ruby/set.rb
@@ -4,8 +4,30 @@
 
 module StrukturData
   class Set
-    def empty?
-      true
-    end
+      def initialize
+        @nothing = true
+        @container = Hash.new(false)
+      end
+
+      def empty?
+        @container.empty?
+      end
+
+      def add(item)
+        return false if contains(item)
+        @container[item] = true
+      end
+
+      def size
+        @container.size
+      end
+
+      def contains(item)
+        @container[item]
+      end
+
+      def remove(item)
+        @container.delete item
+      end
   end
 end
--- ruby/spec/set_spec.rb
+++ ruby/spec/set_spec.rb
@@ -1,5 +1,68 @@
-require_relative '../set'
+# to run:
+# from parent folder
+# $ rspec -fd
 
-RSpec.describe StrukturData::Set do
-  it { expect(subject.empty?).to eql true}
+require_relative '../set'
+module StrukturData
+  RSpec.describe Set do
+    before do
+        @kosong = Set.new
+
+        @satu = Set.new
+        @satu.add "Ferrari"
+
+        @banyak = Set.new
+        @banyak.add "Ferrari"
+        @banyak.add "Mercedes"
+        @banyak.add "Lamborghini"
+    end
+    # it { expect(subject.empty?).to eql true}
+    describe "#empty?" do
+      it "check the collection" do
+        expect(@kosong.empty?).to eq true
+        expect(@satu.empty?).to eq false
+        expect(@banyak.empty?).to eq false
+      end
+    end
+
+    describe '#size' do
+      it "return the size as added" do
+        expect(@kosong.size).to eq 0
+        expect(@satu.size).to eq 1
+        expect(@banyak.size).to eq 3
+      end
+    end
+
+    describe '#contains' do
+      it "check whether the container contains previously added object" do
+        expect(@kosong.contains "Ferrari").to eq false
+        expect(@kosong.contains "Mercedes").to eq false
+
+        expect(@satu.contains "Ferrari").to eq true
+        expect(@satu.contains "Mercedes").to eq false
+
+        expect(@banyak.contains "Ferrari").to eq true
+        expect(@banyak.contains "Mercedes").to eq true
+      end
+    end
+
+    describe '#add' do
+      it "prevent duplicate" do
+        foo = Set.new
+        expect(foo.add "bar").to eq true
+        expect(foo.add "bar").to eq false
+      end
+    end
+
+    describe '#remove' do
+      it "remove specified object" do
+        expect(@banyak.contains "Ferrari").to eq true
+        @banyak.remove("Ferrari")
+        @banyak.remove("any string does not exist")
+        expect(@banyak.contains "Ferrari").to eq false
+        expect(@banyak.contains "Mercedes").to eq true
+
+      end
+    end
+  end
 end
